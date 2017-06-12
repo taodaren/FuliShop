4310f60f853f52b29923edf124d6a1273707a48b
taodaren <taodaren1125@gmail.com>
2017-06-12 18:30:31 +0800

    使用 PagerBottomTabStrip 框架实现底部导航栏

--- app/src/main/java/io/github/taodaren/fulishop/MainActivity.java
+++ app/src/main/java/io/github/taodaren/fulishop/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.taodaren.fulishop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.majiajie.pagerbottomtabstrip.NavigationController;
+import me.majiajie.pagerbottomtabstrip.PageBottomTabLayout;
+import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //创建 Material 风格导航栏
+        setTabBottom();
+    }
+
+    private void setTabBottom() {
+        PageBottomTabLayout tab = (PageBottomTabLayout) findViewById(R.id.tab_bottom_layout);
+
+        //对导航栏进行后续的控制
+        NavigationController navigationController = tab.material()
+                .addItem(R.drawable.tab_new_goods_selected, "新品")
+                .addItem(R.drawable.tab_fine, "精选")
+                .addItem(R.drawable.tab_sort, "分类")
+                .addItem(R.drawable.tab_cart, "购物车")
+                .addItem(R.drawable.tab_mine, "我的")
+                .build();
+
+        //监听导航栏的 Item 选中事件
+        navigationController.addTabItemSelectedListener(new OnTabItemSelectedListener() {
+            @Override
+            public void onSelected(int index, int old) {
+                //选中时触发
+
+            }
+
+            @Override
+            public void onRepeat(int index) {
+                //重复选中时触发
+
+            }
+        });
     }
 }
