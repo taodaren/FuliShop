d7e8eae566fda94fc8723743035ca04d8b75387d
taodaren <taodaren1125@gmail.com>
2017-06-12 19:25:20 +0800

    自定义底部导航栏

--- app/src/main/java/io/github/taodaren/fulishop/MainActivity.java
+++ app/src/main/java/io/github/taodaren/fulishop/MainActivity.java
@@ -1,10 +1,13 @@
 package io.github.taodaren.fulishop;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import me.majiajie.pagerbottomtabstrip.NavigationController;
 import me.majiajie.pagerbottomtabstrip.PageBottomTabLayout;
+import me.majiajie.pagerbottomtabstrip.item.BaseTabItem;
+import me.majiajie.pagerbottomtabstrip.item.NormalItemView;
 import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectedListener;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,13 +24,12 @@ public class MainActivity extends AppCompatActivity {
     private void setTabBottom() {
         PageBottomTabLayout tab = (PageBottomTabLayout) findViewById(R.id.tab_bottom_layout);
 
-        //对导航栏进行后续的控制
-        NavigationController navigationController = tab.material()
-                .addItem(R.drawable.tab_new_goods_selected, "新品")
-                .addItem(R.drawable.tab_fine, "精选")
-                .addItem(R.drawable.tab_sort, "分类")
-                .addItem(R.drawable.tab_cart, "购物车")
-                .addItem(R.drawable.tab_mine, "我的")
+        NavigationController navigationController = tab.custom()
+                .addItem(newItem(R.drawable.tab_new_goods, R.drawable.tab_new_goods_selected, "新品"))
+                .addItem(newItem(R.drawable.tab_fine, R.drawable.tab_fine_selected, "精选"))
+                .addItem(newItem(R.drawable.tab_sort, R.drawable.tab_sort_selected, "分类"))
+                .addItem(newItem(R.drawable.tab_cart, R.drawable.tab_cart_selected, "购物车"))
+                .addItem(newItem(R.drawable.tab_mine, R.drawable.tab_mine_selected, "我的"))
                 .build();
 
         //监听导航栏的 Item 选中事件
@@ -44,5 +46,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    /**
+     * 自定义一个Item
+     */
+    private BaseTabItem newItem(int drawable, int checkedDrawable, String text) {
+        NormalItemView normalItemView = new NormalItemView(this);
+        normalItemView.initialize(drawable, checkedDrawable, text);
+        normalItemView.setTextDefaultColor(Color.GRAY);
+        normalItemView.setTextCheckedColor(0xFFFF5300);
+        return normalItemView;
     }
 }
