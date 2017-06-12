25357f1c762e7f91967e5ec9b3de9abba45d64df
taodaren <taodaren1125@gmail.com>
2017-06-12 17:09:15 +0800

    设置闪屏界面跳转，时间为 2000 毫秒，并去掉顶部标题栏

---
+++ app/src/main/java/io/github/taodaren/fulishop/SplashActivity.java
@@ -0,0 +1,27 @@
+package io.github.taodaren.fulishop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
